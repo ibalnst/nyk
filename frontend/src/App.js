e3b85baaf27aa842b1925d83b2233648d07c8335
ibalnst <ibaldoci@gmail.com>
2021-08-27 13:45:37 +0700

    added smooth transition

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { AnimatedSwitch, spring } from 'react-router-transition';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { Container } from 'react-bootstrap';
@@ -18,47 +20,90 @@ import UserEditScreen from './screens/UserEditScreen';
 import ProductListScreen from './screens/ProductListScreen';
 import ProductEditScreen from './screens/ProductEditScreen';
 import OrderListScreen from './screens/OrderListScreen';
+
 const App = () => {
+  // we need to map the `scale` prop we define below
+  // to the transform style property
+  function mapStyles(styles) {
+    return {
+      opacity: styles.opacity,
+      transform: `scale(${styles.scale})`,
+    };
+  }
+
+  // wrap the `spring` helper to use a bouncy config
+  function bounce(val) {
+    return spring(val, {
+      stiffness: 330,
+      damping: 22,
+    });
+  }
+
+  // child matches will...
+  const bounceTransition = {
+    // start in a transparent, upscaled state
+    atEnter: {
+      opacity: 0,
+      scale: 1.2,
+    },
+    // leave in a transparent, downscaled state
+    atLeave: {
+      opacity: bounce(0),
+      scale: bounce(0.8),
+    },
+    // and rest at an opaque, normally-scaled state
+    atActive: {
+      opacity: bounce(1),
+      scale: bounce(1),
+    },
+  };
   return (
     <Router>
       <Header />
       <main className='py-3'>
         <Container>
           <Switch>
-            <Route path='/login' component={LoginScreen} />
-            <Route path='/order/:id' component={OrderScreen} />
-            <Route path='/placeorder' component={PlaceOrderScreen} />
-            <Route path='/product/:id' component={ProductScreen} />
-            <Route path='/payment' component={PaymentScreen} />
-            <Route path='/cart/:id?' component={CartScreen} />
-            <Route path='/register' component={RegisterScreen} />
-            <Route path='/profile' component={ProfileScreen} />
-            <Route path='/shipping' component={ShippingScreen} />
-            <Route path='/admin/userlist' component={UserListScreen} />
-            <Route path='/admin/user/:id/edit' component={UserEditScreen} />
-            <Route
-              path='/admin/productlist'
-              component={ProductListScreen}
-              exact
-            />
-            <Route
-              path='/admin/productlist/:pageNumber'
-              component={ProductListScreen}
-              exact
-            />
-            <Route path='/admin/orderlist' component={OrderListScreen} />
-            <Route
-              path='/admin/product/:id/edit'
-              component={ProductEditScreen}
-            />
-            <Route path='/search/:keyword' component={HomeScreen} />
-            <Route path='/page/:pageNumber' component={HomeScreen} exact />
-            <Route
-              path='/search/:keyword/page/:PageNumber'
-              component={HomeScreen}
-              exact
-            />
-            <Route path='/' component={HomeScreen} exact />
+            <AnimatedSwitch
+              atEnter={bounceTransition.atEnter}
+              atLeave={bounceTransition.atLeave}
+              atActive={bounceTransition.atActive}
+              mapStyles={mapStyles}
+            >
+              <Route path='/login' component={LoginScreen} />
+              <Route path='/order/:id' component={OrderScreen} />
+              <Route path='/placeorder' component={PlaceOrderScreen} />
+              <Route path='/product/:id' component={ProductScreen} />
+              <Route path='/payment' component={PaymentScreen} />
+              <Route path='/cart/:id?' component={CartScreen} />
+              <Route path='/register' component={RegisterScreen} />
+              <Route path='/profile' component={ProfileScreen} />
+              <Route path='/shipping' component={ShippingScreen} />
+              <Route path='/admin/userlist' component={UserListScreen} />
+              <Route path='/admin/user/:id/edit' component={UserEditScreen} />
+              <Route
+                path='/admin/productlist'
+                component={ProductListScreen}
+                exact
+              />
+              <Route
+                path='/admin/productlist/:pageNumber'
+                component={ProductListScreen}
+                exact
+              />
+              <Route path='/admin/orderlist' component={OrderListScreen} />
+              <Route
+                path='/admin/product/:id/edit'
+                component={ProductEditScreen}
+              />
+              <Route path='/search/:keyword' component={HomeScreen} />
+              <Route path='/page/:pageNumber' component={HomeScreen} exact />
+              <Route
+                path='/search/:keyword/page/:PageNumber'
+                component={HomeScreen}
+                exact
+              />
+              <Route path='/' component={HomeScreen} exact />
+            </AnimatedSwitch>
           </Switch>
         </Container>
       </main>
